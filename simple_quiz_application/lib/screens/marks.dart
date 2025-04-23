a5d99cc344b87a097296f2f6e36f5f10f8d25bfc
heetsolanki <heetsolanki2006@gmail.com>
2025-04-23 18:38:49 +0530

    Added marks.dart

---
+++ simple_quiz_application/lib/screens/marks.dart
@@ -0,0 +1,15 @@
+import 'package:simple_quiz_application/exports.dart';
+
+class Marks extends StatefulWidget {
+  const Marks({super.key});
+
+  @override
+  MarksState createState() => MarksState();
+}
+
+class MarksState extends State<Marks> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
