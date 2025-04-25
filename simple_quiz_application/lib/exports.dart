74a7f6a0e63215c2442e67c510259fa4c48504e6
heetsolanki <heetsolanki2006@gmail.com>
2025-04-25 13:39:25 +0530

    Updated exports.dart

--- simple_quiz_application/lib/exports.dart
+++ simple_quiz_application/lib/exports.dart
@@ -1,17 +1,18 @@
 export 'package:flutter/material.dart';
 export 'package:provider/provider.dart';
-export 'package:simple_quiz_application/questions_card.dart';
-export 'package:simple_quiz_application/screens/html/html_beginner_quiz_screen.dart';
-export 'package:simple_quiz_application/screens/html/html_difficult_quiz_screen.dart';
-export 'package:simple_quiz_application/screens/html/html_intermediate_quiz_screen.dart';
-export 'package:simple_quiz_application/screens/java/java_beginner_quiz_screen.dart';
-export 'package:simple_quiz_application/screens/java/java_difficult_quiz_screen.dart';
-export 'package:simple_quiz_application/screens/java/java_intermediate_quiz_screen.dart';
-export 'package:simple_quiz_application/screens/theme_provider.dart';
 export 'package:smooth_list_view/smooth_list_view.dart';
 
+export 'questions_card.dart';
 export 'screens/home_screen.dart';
+export 'screens/html/html_beginner_quiz_screen.dart';
+export 'screens/html/html_difficult_quiz_screen.dart';
+export 'screens/html/html_intermediate_quiz_screen.dart';
 export 'screens/html/html_quiz_screen.dart';
+export 'screens/java/java_beginner_quiz_screen.dart';
+export 'screens/java/java_difficult_quiz_screen.dart';
+export 'screens/java/java_intermediate_quiz_screen.dart';
 export 'screens/java/java_quiz_screen.dart';
+export 'screens/marks.dart';
 export 'screens/quiz_screen.dart';
 export 'screens/settings_screen.dart';
+export 'screens/theme_provider.dart';
